f78c47106d932d8eb1d995485e854f022df8cece
williambl <william@williambl.com>
2025-03-23 12:33:35 +0000

    feat: new ability triggering

--- src/main/java/dev/upcraft/livingplanet/client/tracking/PlayerTrackingScreen.java
+++ src/main/java/dev/upcraft/livingplanet/client/tracking/PlayerTrackingScreen.java
@@ -1,13 +1,23 @@
 package dev.upcraft.livingplanet.client.tracking;
 
+import dev.upcraft.livingplanet.component.LPComponents;
+import dev.upcraft.livingplanet.net.ChasmsPacket;
+import dev.upcraft.livingplanet.net.GetHisAssPacket;
+import dev.upcraft.livingplanet.net.LightningPacket;
+import dev.upcraft.livingplanet.net.ShockwavePacket;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.client.gui.components.Button;
-import net.minecraft.client.gui.components.StringWidget;
+import net.minecraft.client.gui.components.Tooltip;
 import net.minecraft.client.gui.layouts.HeaderAndFooterLayout;
+import net.minecraft.client.gui.layouts.LinearLayout;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 
 public class PlayerTrackingScreen extends Screen {
     public static final String DESELECT_KEY = "gui.living_planet.tracker.deselect";
+    public static final String LIGHTNING_KEY = "gui.living_planet.tracker.lightning";
+    public static final String GET_HIS_ASS_KEY = "gui.living_planet.tracker.get_his_ass";
+    public static final String CHASMS_KEY = "gui.living_planet.tracker.chasms";
     private final Tracker tracker;
     private final HeaderAndFooterLayout layout = new HeaderAndFooterLayout(this);
     private PlayerTrackingList list;
@@ -22,9 +32,32 @@ public class PlayerTrackingScreen extends Screen {
         super.init();
         layout.addTitleHeader(this.title, this.font);
         layout.addToContents(this.list = new PlayerTrackingList(this.minecraft, 200, 200, 30, this.tracker.playerToTrack, this.tracker::track));
-        layout.addToFooter(Button.builder(Component.translatable(DESELECT_KEY), button -> this.list.setSelected(null))
-                .width(100)
+        var footer = layout.addToFooter(LinearLayout.horizontal().spacing(2));
+        footer.addChild(Button.builder(Component.translatable(DESELECT_KEY), button -> this.list.setSelected(null))
+                .width(80)
                 .build());
+        var component = this.minecraft.player.getComponent(LPComponents.LIVING_PLANET);
+        var lightningButton = footer.addChild(Button.builder(Component.translatable(LIGHTNING_KEY), button -> ClientPlayNetworking.send(new LightningPacket()))
+                .width(80)
+                .build());
+        if (!component.canLightning()) {
+            lightningButton.active = false;
+            lightningButton.setTooltip(Tooltip.create(Component.translatable(ShockwavePacket.COOLDOWN_MESSAGE_KEY)));
+        }
+        var getHisAssButton = footer.addChild(Button.builder(Component.translatable(GET_HIS_ASS_KEY), button -> ClientPlayNetworking.send(new GetHisAssPacket()))
+                .width(80)
+                .build());
+        if (!component.canGetHisAss()) {
+            getHisAssButton.active = false;
+            getHisAssButton.setTooltip(Tooltip.create(Component.translatable(ShockwavePacket.COOLDOWN_MESSAGE_KEY)));
+        }
+        var chasmsButton = footer.addChild(Button.builder(Component.translatable(CHASMS_KEY), button -> ClientPlayNetworking.send(new ChasmsPacket()))
+                .width(80)
+                .build());
+        if (!component.canChasms()) {
+            chasmsButton.active = false;
+            chasmsButton.setTooltip(Tooltip.create(Component.translatable(ShockwavePacket.COOLDOWN_MESSAGE_KEY)));
+        }
         layout.visitWidgets(this::addRenderableWidget);
         this.repositionElements();
     }
